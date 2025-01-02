f9f1c108b65d81a374f3b123d7b989436c56e562
jaejae0015 <salangae00@naver.com>
2025-01-02 17:38:07 +0900

    jeckjeck 프로젝트 초기커밋

---
+++ src/main/java/com/jaejaeproject/jeckjeck/JeckjeckApplication.java
@@ -0,0 +1,13 @@
+package com.jaejaeproject.jeckjeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeckjeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeckjeckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaejaeproject/jeckjeck/JeckjeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaejaeproject.jeckjeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeckjeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
